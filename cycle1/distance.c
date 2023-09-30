75ce2714a31c1732e8e9688f46111dc1e72139c6
Anandhu v <anandhupoovankal1@gmail.com>
2023-09-30 14:56:52 +0530

    Completed

--- cycle1/distance.c
+++ cycle1/distance.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 
+
 struct Distance {
-   int feet;
-   float inch;
-} d1, d2, result;
+int feet;
+int inches;
+};
 
-int main() {
-   // take first distance input
-   printf("Enter 1st distance\n");
-   printf("Enter feet: ");
-   scanf("%d", &d1.feet);
-   printf("Enter inch: ");
-   scanf("%f", &d1.inch);
+void main() 
+{
+struct Distance distance1, distance2, result;
+printf("Enter details for the first distance (feet inches): ");
+scanf("%d %d", &distance1.feet, &distance1.inches);
+printf("Enter details for the second distance (feet inches): "); 
+scanf("%d %d", &distance2.feet, &distance2.inches);
  
-   // take second distance input
-   printf("\nEnter 2nd distance\n");
-   printf("Enter feet: ");
-   scanf("%d", &d2.feet);
-   printf("Enter inch: ");
-   scanf("%f", &d2.inch);
-   
-   // adding distances
-   result.feet = d1.feet + d2.feet;
-   result.inch = d1.inch + d2.inch;
-
-   // convert inches to feet if greater than 12
-   while (result.inch >= 12.0) {
-      result.inch = result.inch - 12.0;
-      ++result.feet;
-   }
-   printf("\nSum of distances = %d\'-%.1f\"", result.feet, result.inch);
-   return 0;
+ result.inches = distance1.inches + distance2.inches;
+ result.feet = distance1.feet + distance2.feet + (result.inches / 12);
+result.inches %= 12;
+ printf("Resultant distance: %d feet %d inches \n", result.feet, result.inches);
 }
