4bc0d15d6583a0cc9b1d146ce48148fb56207eeb
shaibinkb16 <shaibinkb16@gmail.com>
2023-10-19 15:58:49 +0530

    Completed

---
+++ cycle2/setOperations.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+int readSet(int set[]) {
+    int size, i;
+
+    printf("Enter the size of the set: ");
+    scanf("%d", &size);
+
+    printf("Enter elements of the set:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &set[i]);
+    }
+
+    return size;
+}
+
+void displaySet(int set[], int size) {
+    int i;
+
+    printf("Set: { ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", set[i]);
+    }
+    printf("}\n");
+}
+
+int setUnion(int set1[], int size1, int set2[], int size2, int result[]) {
+    int i, j, k = 0;
+
+    for (i = 0; i < size1; i++) {
+        result[k++] = set1[i];
+    }
+
+    for (i = 0; i < size2; i++) {
+        int found = 0;
+        for (j = 0; j < size1; j++) {
+            if (set2[i] == set1[j]) {
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            result[k++] = set2[i];
+        }
+    }
+
+    return k;
+}
+
+int setIntersection(int set1[], int size1, int set2[], int size2, int result[]) {
+    int i, j, k = 0;
+
+    for (i = 0; i < size1; i++) {
+        for (j = 0; j < size2; j++) {
+            if (set1[i] == set2[j]) {
+                result[k++] = set1[i];
+                break;
+            }
+        }
+    }
+
+    return k;
+}
+
+int setDifference(int set1[], int size1, int set2[], int size2, int result[]) {
+    int i, j, k = 0;
+
+    for (i = 0; i < size1; i++) {
+        int found = 0;
+        for (j = 0; j < size2; j++) {
+            if (set1[i] == set2[j]) {
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            result[k++] = set1[i];
+        }
+    }
+
+    return k;
+}
+
+int main() {
+    int set1[MAX_SIZE], set2[MAX_SIZE], result[MAX_SIZE];
+    int size1, size2, resultSize;
+    int choice;
+
+    size1 = readSet(set1);
+    size2 = readSet(set2);
+
+    do {
+        printf("\nSet Operations:\n");
+        printf("1. Union\n");
+        printf("2. Intersection\n");
+        printf("3. Difference (set1 - set2)\n");
+        printf("4. Display Sets\n");
+        printf("5. Quit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                resultSize = setUnion(set1, size1, set2, size2, result);
+                displaySet(result, resultSize);
+                break;
+
+            case 2:
+                resultSize = setIntersection(set1, size1, set2, size2, result);
+                displaySet(result, resultSize);
+                break;
+
+            case 3:
+                resultSize = setDifference(set1, size1, set2, size2, result);
+                displaySet(result, resultSize);
+                break;
+
+            case 4:
+                printf("\nSets:\n");
+                printf("Set 1: ");
+                displaySet(set1, size1);
+                printf("Set 2: ");
+                displaySet(set2, size2);
+                break;
+
+            case 5:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    } while (choice != 5);
+
+    return 0;
+}
---
+++ cycle2/treeTraversal.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+void inOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+
+void preOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
+
+
+void postOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+
+void breadthFirstSearch(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    // Initialize a queue for BFS
+    struct Node* queue[100];
+    int front = -1, rear = -1;
+
+    // Enqueue the root node
+    queue[++rear] = root;
+
+    while (front < rear) {
+        struct Node* current = queue[++front];
+        printf("%d ", current->data);
+
+        if (current->left != NULL)
+            queue[++rear] = current->left;
+
+        if (current->right != NULL)
+            queue[++rear] = current->right;
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    int choice, value;
+
+    do {
+        printf("\nBinary Tree Operations:\n");
+        printf("1. Insert\n");
+        printf("2. In-order Traversal\n");
+        printf("3. Pre-order Traversal\n");
+        printf("4. Post-order Traversal\n");
+        printf("5. Breadth-First Search (BFS)\n");
+        printf("6. Quit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
+
+            case 2:
+                printf("In-order Traversal: ");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
+
+            case 3:
+                printf("Pre-order Traversal: ");
+                preOrderTraversal(root);
+                printf("\n");
+                break;
+
+            case 4:
+                printf("Post-order Traversal: ");
+                postOrderTraversal(root);
+                printf("\n");
+                break;
+
+            case 5:
+                printf("Breadth-First Search (BFS): ");
+                breadthFirstSearch(root);
+                printf("\n");
+                break;
+
+            case 6:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    } while (choice != 6);
+
+    return 0;
+}
